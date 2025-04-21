a17b26e0fef4263ad87a738e676c67d74a1a1074
Bozadji Artiom <146107079+fish30008@users.noreply.github.com>
2025-04-21 18:54:26 +0300

    Update Bozadji_Artiom_please_cooperate_round_3.py

--- Bozadji_Artiom_please_cooperate_round_3.py
+++ Bozadji_Artiom_please_cooperate_round_3.py
@@ -1,60 +1,65 @@
-def strategy_round_(opponent_id: int, my_history: dict[int, list[int]], opponents_history: dict[int, list[int]]) -> tuple[int, int]:
-        
-    try:
-        my_moves = my_history[opponent_id]
-        opponent_moves = opponents_history[opponent_id]
-        move = 0  
-        if not opponent_moves:
+def strategy_round_3(opponent_id: int, my_history: dict[int, list[int]], opponents_history: dict[int, list[int]]) -> tuple[int, int]:
+    my_moves = my_history[opponent_id]
+    opponent_moves = opponents_history[opponent_id]
+    move = 0 
+    
+    if not opponent_moves:
+        move = 0
+    else:
+        cooperation, rejection = 0, 0
+        for i in opponent_moves:
+            if i == 0:
+                rejection += 1
+            else:
+                cooperation += 1
+        if len(my_moves) >= 2 and my_moves[-2:] == [1, 0]:
             move = 0
+        elif 95 <= len(opponent_moves) < 100:
+            move = 0
+        elif cooperation == 0 and rejection < 25:
+            move = 1
+        elif rejection - cooperation == 21:
+            move = 1
+        elif len(opponent_moves) >= 10 and all(move == 0 for move in opponent_moves[-10:]):
+            move = 1
         else:
-            cooperation, rejection = 0, 0
-            for i in opponent_moves:
-                if i == 0:
-                    rejection += 1
-                else:
-                    cooperation += 1
-            if len(my_moves) >= 2 and my_moves[-2:] == [1, 0]:
-                move = 0
-            elif 95 <= len(opponent_moves) < 100:
-                move = 0
-            elif cooperation == 0 and rejection < 25:
-                move = 1
-            elif rejection - cooperation == 21:
-                move = 1
-            elif len(opponent_moves) >= 10 and all(move == 0 for move in opponent_moves[-10:]):
-                move = 1
-            else:
-                move = 0
-        
-        next_opponent_candidates = []
-        
-        for opp_id in my_history:
-            if len(my_history[opp_id]) >= 200:
-                continue
-                
-            opp_moves = opponents_history[opp_id]
-            if not opp_moves:
-                next_opponent_candidates.append(opp_id)
-            else:
-                coop_rate = sum(opp_moves) / len(opp_moves) if opp_moves else 0
-                
+            move = 0
     
-                if coop_rate > 0.7:
-                    next_opponent_candidates.append(opp_id)
-                    next_opponent_candidates.append(opp_id)  
+    best_opponent = opponent_id 
+    best_score = -1
     
-                elif opp_moves and (sum(1 for m in opp_moves if m == 0) - sum(1 for m in opp_moves if m == 1)) in [20, 21, 22]:
-                    next_opponent_candidates.append(opp_id)
-                    next_opponent_candidates.append(opp_id) 
-                else:
-                    next_opponent_candidates.append(opp_id)
+    for opp_id in my_history:
+        if len(my_history[opp_id]) >= 200:
+            continue
+            
+        opp_moves = opponents_history[opp_id]
+        score = 0
         
-        # If no eligible opponents, use current opponent
-        if not next_opponent_candidates:
-            next_opponent = opponent_id
+        if not opp_moves:
+            score = 50
         else:
-            next_opponent = opponent_id + 1
-    except:
-        return 0, idk
+            coop_rate = sum(opp_moves) / len(opp_moves)
+            
+            if coop_rate > 0.7:
+                score = 90
+
+            elif len(opp_moves) > 0:
+                defect_diff = sum(1 for m in opp_moves if m == 0) - sum(1 for m in opp_moves if m == 1)
+                if 19 <= defect_diff <= 23:  
+                    score = 80
+                else:
+                    score = int(coop_rate * 60)
+        
+
+        round_total = sum(len(hist) for hist in my_history.values())
+        score = (score + (opp_id * 7 + round_total * 13) % 20) % 100
+        
+        rounds_played = len(my_history[opp_id])
+        if rounds_played < 50:
+            score += 10
+
+        if score > best_score:
+            best_score = score
+            best_opponent = opp_id
     
-    return move, next_opponent
+    return move, best_opponent
